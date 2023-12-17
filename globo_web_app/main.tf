b7790d6deca68e7010a5d956f5aa6c2c4a6e46e7
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-12-17 14:30:33 +0000

    creating ec2 instance

---
+++ globo_web_app/main.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "web_server" {
+  ami = "ami-0fc5d935ebf8bc3bc"
+  instance_type = var.instance_type
+   tags = {
+    Name = "terraform-example"
+  }
+
+}
