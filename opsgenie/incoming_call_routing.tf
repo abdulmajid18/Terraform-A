c758933ceb90da420839213da766f688e7f08de9
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-12-19 19:13:47 +0000

    added incoming call routing

---
+++ opsgenie/incoming_call_routing.tf
@@ -0,0 +1,14 @@
+resource "opsgenie_api_integration" "call_routing_integration" {
+  name = "Call Routing Integration"
+
+  responders {
+    type = "user"
+    id   = "${opsgenie_user.ross.id}"
+  }
+
+  enabled                        = true
+  allow_write_access             = true
+  ignore_responders_from_payload = true
+  suppress_notifications         = true
+  owner_team_id                  = "${opsgenie_team.opsgenie_scom_team.id}"
+}
